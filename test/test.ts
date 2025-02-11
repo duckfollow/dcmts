e57a65c2eb31b96157c5eeb5a316ca24a4a7e9a6
prasit <prasit.su@bdms.co.th>
2025-02-11 16:50:28 +0700

    update

--- test/test.ts
+++ test/test.ts
@@ -11,7 +11,24 @@ import {
       console.log(await checkDCMTKVersion());
       console.log(await readDICOMMetadata("./test/example.dcm"));
       console.log(await convertDICOMToPNG("./test/example.dcm", "./test/output.png"));
-      console.log(await convertJPGToDICOM("./test/input.jpg", "./test/output.dcm", { PatientName: "John^Doe" }));
+      console.log(await convertJPGToDICOM("./test/input.jpg", "./test/output.dcm", {
+        PatientName: "John Test",
+        PatientID: "123456",
+        PatientBirthDate: "19700101",
+        PatientSex: "M",
+        StudyInstanceUID: "1.2.3.4.5.6.7.8",
+        StudyDate: "20250207",
+        StudyTime: "120000",
+        StudyDescription: "CT Head",
+        SeriesInstanceUID: "1.2.3.4.5.6.7.9",
+        SeriesDate: "20020628",
+        SeriesDescription: "Brain Scan",
+        Modality: "CT",
+        BodyPartExamined: "HEAD",
+        SOPInstanceUID: "1.2.3.4.5.6.7.10",
+        // PhotometricInterpretation: "RGB", //MONOCHROME2
+        // "ScanningSequence": "GR",
+      }));
       // console.log(await sendDICOMToPACS("192.168.1.100", "104", "./test/example.dcm"));
     } catch (error) {
       console.error("Error:", error);
